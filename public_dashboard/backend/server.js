bdf17fce1ea7eb149de45c7120a1318ec22868e8
Gabriele <gabrieletuozzo@gmail.com>
2026-03-04 16:51:15 +0000

    updated CORS configuration and modified dashboard resource IDs; changed homepage in package.json

--- public_dashboard/backend/server.js
+++ public_dashboard/backend/server.js
@@ -4,14 +4,31 @@ const jwt = require("jsonwebtoken");
 const cors = require('cors');
 
 const app = express();
-app.use(cors()); 
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+  credentials: true
+}));
+
 port = process.env.PORT || 5000;
 const METABASE_SITE_URL = process.env.METABASE_SITE_URL;
 const METABASE_SECRET_KEY = process.env.METABASE_SECRET_KEY;
 
 app.get("/nodedash/metabase-dashboard", (req, res) => {
   const payload = {
-    resource: { dashboard: 9 },
+    resource: { dashboard: 3 },
+    params: {},
+    exp: Math.round(Date.now() / 1000) + 10 * 60, // 10-minute expiration
+  };
+
+  const token = jwt.sign(payload, METABASE_SECRET_KEY);
+  const iframeUrl = `${METABASE_SITE_URL}/embed/dashboard/${token}#bordered=true&titled=true`;
+  res.json({ iframeUrl });
+});
+
+app.get("/nodedash/research-profiles", (req, res) => {
+  const payload = {
+    resource: { dashboard: 4 },
     params: {},
     exp: Math.round(Date.now() / 1000) + 10 * 60, // 10-minute expiration
   };
